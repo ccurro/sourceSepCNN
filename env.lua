4f86f11530c25d64f280b771878471ce5794a731
Chris Curro <curro@cooper.edu>
2015-03-18 10:14:49 -0400

    added fork of nnx.SuperCriterion, removed nnx from env for time being

--- env.lua
+++ env.lua
@@ -1,7 +1,12 @@
 require 'nn'
 require 'cutorch'
 require 'cunn'
-require 'nnx'
+-- require 'nnx'
+--[[
+	Disabled nnx while we work on a fork of 
+	nnx.SuperCriterion() due to it's limitations
+	working with table criteria.
+]]
 require 'audio'
 require 'signal'
 require 'gnuplot'
